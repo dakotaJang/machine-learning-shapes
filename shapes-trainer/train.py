2f3e2ad2a2613b8d4eefca4e3c1e2ce18f0b68e2
dakotaJang <dev@dakotajang.com>
2018-07-10 16:37:51 -0700

    dump labels.json and let user choose subdir for shapes-data

--- shapes-trainer/train.py
+++ shapes-trainer/train.py
@@ -5,35 +5,47 @@ import tensorflowjs as tfjs
 import os
 import numpy as np
 import time
+import json
+import sys
 
 import training_shapes_module as tm
 
-image_data_dir = "shapes-data/"
-image_data = "convex_polygon_inscribed_in_ellipse"
-image_dir = image_data_dir + image_data
+# If user wants to specify sub-directory for their data they can specify as an argument
+# else use root directory of shapes-data
+if len(sys.argv) == 2:
+  image_data_dir = sys.argv[1]
+else:
+  image_data_dir = ""
+
+image_data_service_dir = "shapes-data/"
+image_dir = image_data_service_dir + image_data_dir
+BATCH_SIZE = 16
+SHUFFLE_SIZE = 1000
+EPOCHS=50
 
 IMAGE_FORMAT = {'height':100,'width':100}
 
-NUM_OF_SHAPES = tm.prepare_data(image_dir)
-BATCH_SIZE = 16
-SHUFFLE_SIZE = 1000
+LABELS = tm.prepare_data(image_dir)
+NUM_OF_LABELS = len(LABELS)
 
-EPOCHS=50
+def add_labels_json(path):
+  with open(path+"/labels.json", 'w') as f:
+    json.dump({'labels': LABELS}, f)
 
 def simple():
   # Create the dataset
-  train_dataset, test_dataset = tm.load_data(image_dir, number_of_outputs=NUM_OF_SHAPES, flatten=True, batch_size=BATCH_SIZE, shuffle_size=SHUFFLE_SIZE)
+  train_dataset, test_dataset = tm.load_data(image_dir, number_of_outputs=NUM_OF_LABELS, flatten=True, batch_size=BATCH_SIZE, shuffle_size=SHUFFLE_SIZE)
   # Create model
-  model = tm.simple_model(IMAGE_FORMAT['height'],IMAGE_FORMAT['width'],NUM_OF_SHAPES)
+  model = tm.simple_model(IMAGE_FORMAT['height'],IMAGE_FORMAT['width'],NUM_OF_LABELS)
   # train and test model
   tm.train_and_test(model,EPOCHS,train_dataset,test_dataset)
   return model
 
 def conv():
   # Create the dataset
-  train_dataset, test_dataset = tm.load_data(image_dir, number_of_outputs=NUM_OF_SHAPES, batch_size=BATCH_SIZE, shuffle_size=SHUFFLE_SIZE)
+  train_dataset, test_dataset = tm.load_data(image_dir, number_of_outputs=NUM_OF_LABELS, batch_size=BATCH_SIZE, shuffle_size=SHUFFLE_SIZE)
   # Create model
-  model = tm.cnn_model(IMAGE_FORMAT['height'],IMAGE_FORMAT['width'],NUM_OF_SHAPES)
+  model = tm.cnn_model(IMAGE_FORMAT['height'],IMAGE_FORMAT['width'],NUM_OF_LABELS)
   # train and test model
   tm.train_and_test(model,EPOCHS,train_dataset,test_dataset)
   return model
@@ -43,16 +55,20 @@ def main():
   simple_model = simple()
   print('Finished training simple network')
   print('Saving simple network')
-  simple_model.save('shapes-model/simple_model_'+image_data+time.strftime('%Y%m%d_%H%M')+'.h5')
-  tfjs.converters.save_keras_model(simple_model, 'shapes-model/simple_model_'+image_data+time.strftime('%Y%m%d_%H%M'))
+  model_path = 'shapes-model/simple_model_'+image_data_dir+time.strftime('%Y%m%d_%H%M')
+  simple_model.save(model_path+'.h5')
+  tfjs.converters.save_keras_model(simple_model, model_path)
+  add_labels_json(model_path)
   print('Saved simple network')
 
   print('Start training Convoluted network:')
   conv_model = conv()
   print('Finished training convoluted network')
   print('Saving convoluted network')
-  conv_model.save('shapes-model/conv_model_'+image_data+time.strftime('%Y%m%d_%H%M')+'.h5')
-  tfjs.converters.save_keras_model(conv_model, 'shapes-model/conv_model_'+image_data+time.strftime('%Y%m%d_%H%M'))
+  model_path = 'shapes-model/conv_model_'+image_data_dir+time.strftime('%Y%m%d_%H%M')
+  conv_model.save(model_path+'.h5')
+  tfjs.converters.save_keras_model(conv_model, model_path)
+  add_labels_json(model_path)
   print('Saved convoluted network')
 
 if __name__ == "__main__":
